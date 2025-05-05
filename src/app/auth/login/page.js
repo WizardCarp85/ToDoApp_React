dd3e61c38ba60fd4a54d4ffa81eb5b6167c530f2
Yashkumar Nimje <yashkumarnimje@Yashkumars-MacBook-Pro.local>
2025-05-06 03:44:44 +0530

    navbar phase 2 done

---
+++ src/app/auth/login/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='login mt-20'>
+
+    </div>
+  )
+}
+
+export default Login
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,31 @@
 import Link from "next/link";
 import React from "react";
-import styles from './Navbar.module.css';
-
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
     <div className={`${styles.navbar} sm:h-16 lg:h-24 rounded-xl m-2`}>
       <div className={`${styles.logo}`}>
-      <img src="/logo.png" alt="logo" className={styles.logo_img}/>
-      <span>Pixel Planner</span>
+        <Link href="../home">
+          <img src="/logo.png" alt="logo" className={styles.logo_img} />
+          </Link>
+          <Link href="../home">
+          <span>Pixel Planner</span>
+        </Link>
       </div>
       <div className={styles.linksdiv}>
-        <Link href="../home" className={styles.links}>Home</Link>
-        <Link href="../create" className={styles.links}>Create ✨</Link>
-        <Link href="../dashboard" className={styles.links}>PlannerHub</Link>
+        <Link href="../home" className={styles.links}>
+          Home
+        </Link>
+        <Link href="../create" className={styles.links}>
+          Create ✨
+        </Link>
+        <Link href="../dashboard" className={styles.links}>
+          PlannerHub
+        </Link>
+        
         {/* temp for login button */}
-        <h3 className={styles.auth}>Login</h3>
+        <Link href="../auth/login" className={styles.auth} >Login</Link>
       </div>
     </div>
   );
