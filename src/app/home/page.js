716698aa792e67ba5fa82a943c31347955588a7d
Yashkumar Nimje <yashkumarnimje@Yashkumars-MacBook-Pro.local>
2025-06-28 14:58:04 +0530

    home page done

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,9 +1,103 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    AOS.init({ duration: 900, once: false });
+  }, []);
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      {/* heading */}
+      <div
+        className="heading flex items-center justify-center my-[10em] md:my-[22em]"
+        data-aos="fade-up"
+      >
+        <h1 className="text-3xl md:text-6xl font-semibold md:font-bold text-[#B7E6FE]">
+          🎮 Welcome to Pixel Planner!
+        </h1>
+      </div>
+      {/* info */}
+      <div
+        className="info flex flex-col items-center justify-center text-[#D1D5DB] mx-[3em] text-center gap-10 mb-[4em] text-lg md:text-3xl md:mx-[10em] md:gap-28"
+        data-aos="fade-up"
+      >
+        <p>
+          ✨ Your ultimate quest to conquer chaos begins here. 🧠 Whether you’re
+          battling deadlines or navigating daily dungeons, Pixel Planner is your
+          loyal sidekick.
+        </p>
+        <p>
+          ⚔️ Craft your perfect day, equip tasks like gear, and level up your
+          productivity! 🎯 From epic goals to daily quests — plan it all, your
+          way.
+        </p>
+        <p>🗡️🛡️ Let’s slay that to-do list, hero. Adventure awaits!</p>
+      </div>
+      {/* redirect button */}
+      <div
+        className="redirect bg-[#004aad] hover:bg-[#007acc] border-2 border-cyan-400 rounded-xl my-[10em] mx-[6em] md:mx-[24em] md:py-3 px-4 py-2 text-center md:text-2xl font-semibold hover:scale-105"
+        data-aos="zoom-in-up"
+      >
+        <h1>
+          <Link href="/create">Start Conquering Chaos 🚀</Link>
+        </h1>
+      </div>
+      {/* extrainfo */}
+      <div className="extrainfo" data-aos="fade-up">
+        <h1 className="text-2xl md:text-5xl text-center md:mb-20 text-sky-400 font-bold">
+          ⚙️ Level Up With These Perks
+        </h1>
+        <div className="my-12 mx-8 text-lg md:text-2xl text-center">
+          <ul className="space-y-5 md:space-y-10">
+            <li>📅 Plan your missions.</li>
+            <li>🎯 Complete your goals like a true champion.</li>
+            <li>⚡ Stay on top of deadlines and conquer tasks.</li>
+            <li>🛡️ Shield your productivity with smart planning.</li>
+          </ul>
+        </div>
+      </div>
+      {/* reviews */}
+      <div
+        className="reviews bg-gray-800 py-4 px-4 md:px-8 rounded-lg shadow-lg text-center text-white mt-20 mx-12 mb-20 md:mx-[24em] md:my-[8em] "
+        data-aos="zoom-in"
+      >
+        <h2 className="text-xl md:text-3xl font-semibold">
+          What Our Users Say
+        </h2>
+        <div className="mt-6 space-y-8">
+          <div className="flex flex-col items-center">
+            <p className="text-md md:text-xl italic">
+              &quot;Pixel Planner has transformed the way I organize my day.
+              It’s like having a personal sidekick!
+            </p>
+            <h4 className="mt-2 text-xl font-semibold">WizardCarp</h4>
+            <p className="text-md text-gray-400">Software Engineer</p>
+          </div>
+        </div>
+      </div>
+      {/* Community Section*/}
+      <div
+        className="community bg-[#004aad] p-6 rounded-lg shadow-lg text-center text-white mb-20 mt-20 mx-12 md:mx-[24em]"
+        data-aos="zoom-in"
+      >
+        <h2 className="text-xl md:text-3xl font-semibold">
+          Join the Pixel Planner Community
+        </h2>
+        <p className="text-sm md:text-xl mt-4">
+          Connect with other heroes, share your progress, and stay updated!
+        </p>
+        <div className="mt-6">
+          <button className="community bg-[#007acc] hover:bg-[#0066b3] px-4 py-2 md:px-6 md:py-3 rounded-xl text-white text-md md:text-xl font-semibold transition duration-300">
+            Join Now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,10 +7,12 @@ const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="flex flex-row justify-around">
-      <div className="navbar md:flex md:justify-between md:items-center absolute w-[99.8%] top-0 bg-[#0F0F0F]/60 h-[86px] md:h-24 pt-1 pl-8 md:pl-12 flex justify-between border-b-4 border-[#484848] rounded-b-xl">
+      {/* Logo */}
+      <div className="fixed md:flex z-100 md:justify-between md:items-center w-[99.8%] top-0 bg-[#0F0F0F]/90 h-[86px] md:h-24 pt-1 pl-8 md:pl-12 flex justify-between border-b-4 border-[#484848] rounded-b-xl">
         <Link
           href="/"
           className="flex items-center gap-2 text-[#0091FF] font-semibold"
+          onClick={() => setMenuOpen(false)}
         >
           <Image
             src="/logo.png"
@@ -21,12 +23,34 @@ const Navbar = () => {
           />
           <span className="text-[2em] md:text-[3rem] pt-1">Pixel Planner</span>
         </Link>
+        {/* desktop navbar */}
         <div className="hidden md:flex md:gap-16 text-[1.6rem] text-[#D8D8D8] font-semibold md:pr-16">
-          <Link href="/home">Home</Link>
-          <Link href="/create">Create✨</Link>
-          <Link href="/dashboard">Dashboard</Link>
-          <Link href="/login">Login</Link>
+          <Link
+            href="/home"
+            className="hover:scale-[1.15] transition-transform duration-[400ms] ease-in-out hover:text-[#0099ff]"
+          >
+            Home
+          </Link>
+          <Link
+            href="/create"
+            className="hover:scale-[1.15] transition-transform duration-[400ms] ease-in-out hover:text-[#0099ff]"
+          >
+            Create✨
+          </Link>
+          <Link
+            href="/dashboard"
+            className="hover:scale-[1.15] transition-transform duration-[400ms] ease-in-out hover:text-[#0099ff]"
+          >
+            Dashboard
+          </Link>
+          <Link
+            href="/login"
+            className="hover:scale-[1.15] transition-transform duration-[400ms] ease-in-out hover:text-[#0099ff]"
+          >
+            Login
+          </Link>
         </div>
+        {/* mobile navbar */}
         <button
           onClick={() => setMenuOpen(!menuOpen)}
           className="text-white md:hidden pr-12 text-2xl"
@@ -34,33 +58,33 @@ const Navbar = () => {
           <i className="fa-solid fa-bars"></i>
         </button>
       </div>
-
+      {/* mobile navbar menu*/}
       <div
-        className={`fixed right-0 z-50 md:hidden w-[70%] h-screen bg-[#0c0c0c] rounded-l-3xl pt-8 pl-10 transition-transform duration-500 ease-in-out ${
+        className={`fixed right-0 z-50 md:hidden w-[70%] h-screen bg-[#0c0c0c]/96 rounded-l-3xl pt-8 pl-10 transition-transform duration-500 ease-in-out ${
           menuOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
-        <div className="flex justify-left items-top h-screen pt-8 pl-3 bg-[#0c0c0c] rounded-l-3xl">
+        <div className="flex justify-left items-top h-screen pt-8 pl-3 rounded-l-3xl">
           <ul className="text-[#e8e8e8] text-2xl leading-16 w-full mr-14">
             <li className="border-b-2 border-dotted">
-              <Link href="/home">
+              <Link href="/home" onClick={() => setMenuOpen(false)}>
                 <i className="fa-regular fa-square-caret-right pr-3"></i>Home
               </Link>
             </li>
             <li className="border-b-2 border-dotted">
-              <Link href="/create">
+              <Link href="/create" onClick={() => setMenuOpen(false)}>
                 <i className="fa-regular fa-square-caret-right pr-3"></i>
                 Create✨
               </Link>
             </li>
             <li className="border-b-2 border-dotted">
-              <Link href="/dashboard">
+              <Link href="/dashboard" onClick={() => setMenuOpen(false)}>
                 <i className="fa-regular fa-square-caret-right pr-3"></i>
                 Dashboard
               </Link>
             </li>
             <li className="border-b-2 border-dotted">
-              <Link href="/login">
+              <Link href="/login" onClick={() => setMenuOpen(false)}>
                 <i className="fa-regular fa-square-caret-right pr-3"></i>Login
               </Link>
             </li>
