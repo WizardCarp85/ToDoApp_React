5596e1d843e0a0a80d09d95e4717d0d4737ffc05
Yashkumar Nimje <yashkumarnimje@Yashkumars-MacBook-Pro.local>
2025-07-18 16:45:47 +0530

    added CRUD functionality

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,62 +1,98 @@
 "use client";
 import Task from "@/components/Task/Task";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import {db} from "@/app/firebase/config";
+import {query,collection, onSnapshot, updateDoc, doc, addDoc, deleteDoc} from "firebase/firestore";
 
 const Dashboard = () => {
-  const [tasks, setTasks] = useState([
-    "learn react",
-    "leetcode",
-  ]);
+  const [tasks, setTasks] = useState([]);
+  const [input,setInput] = useState("")
 
   //create tasks
+  const createTask = async (e) => {
+    e.preventDefault(e)
+    if(input===""){
+      alert('Please enter a valid task')
+      return
+    }
+    await addDoc(collection(db,'tasks'),{
+      text:input,
+      completed:false,
+    })
+    setInput("")
+  }
 
   //read tasks from firebase
+  useEffect(() => {
+    const q = query(collection(db,'tasks'))
+    const snap = onSnapshot(q,(querySnapshot)=>{
+      let tasksArr = []
+      querySnapshot.forEach((elem)=>{
+        tasksArr.push({...elem.data(),id:elem.id})
+      })
+      setTasks(tasksArr)
+    })
+    return () => snap()
+  }, []);
 
   //update tasks in firebase
+  const toggleComplete = async(task) => {
+    await updateDoc(doc(db,'tasks',task.id),{
+      completed : !task.completed
+    })
+
+  }
 
   //delete tasks
+  const deleteTask = async(id) => {
+    await deleteDoc(doc(db,'tasks',id))
+  }
 
 
   return (
-    <div className="dashboard flex flex-col items-center w-full md:gap-6 md:mx-[2em] h-screen">
+    <div className="dashboard flex flex-col items-center md:gap-6 md:mx-[2em] h-screen w-screen">
       {/* heading */}
-      <div className="title flex items-center justify-center md:mt-20">
-        <h1 className="text-[#1e90ff] md:text-4xl font-semibold tracking-wide drop-shadow-[1px_1px_1px_#3b82f6]">
+      <div className="title flex items-center justify-center md:mt-20 mt-10">
+        <h1 className="text-[#1e90ff] md:text-4xl mb-4 text-2xl font-semibold tracking-wide drop-shadow-[1px_1px_1px_#3b82f6]">
           PlannerHub
         </h1>
       </div>
       {/* creating task */}
-      <div className="create md:my-8">
-        <h1 className="md:text-2xl font-semibold mb-2 text-gray-300">
+      <div className="create my-6 md:my-8 w-[70%] md:w-[50%]">
+        <h1 className="md:text-2xl text-xl font-semibold mb-2 text-gray-300">
           Create Task
         </h1>
-        <form className="create-form flex flex-row md:gap-1 md:w-[32em] border-2 rounded-md border-gray-500">
+        <form onSubmit={createTask} className="create-form flex flex-row md:gap-1 border-2 rounded-md border-gray-500">
           <input
             type="text"
+            onChange={(e)=>setInput(e.target.value)}
+            value ={input}
             placeholder="Add a new task . . . ."
-            className="task-input md:px-4 h-[3.2rem] w-full"
+            className="task-input md:px-4 md:h-[3.2rem] w-full text-xs md:text-base p-2 focus:outline-none focus:ring-0"
           />
           <button
             type="submit"
             className="create-button text-blue-500 hover:text-blue-400 md:h-[3.2rem] md:w-[3.2rem] flex items-center justify-center"
           >
-            <i className="fa-solid fa-square-plus text-5xl md:p-1"></i>
+            <i className="fa-solid fa-square-plus text-2xl md:text-5xl md:p-1 p-1"></i>
           </button>
         </form>
       </div>
       {/* modifying task list */}
-      <div className="task-list md:w-[32em]">
-        <h1 className="md:text-2xl font-semibold text-gray-300 w-full md:mb-3">
+      <div className="task-list w-[70%] md:w-[50%]">
+        <h1 className="md:text-2xl text-xl font-semibold text-gray-300 w-full mb-2 md:mb-3">
           My Tasks
         </h1>
         <div className="w-full border-2 border-gray-500 rounded-lg">
-          <ul className="w-full">
+          <ul className="w-full tracking-wider">
             {tasks.map((task, index) => (
-              <Task key={index} task={task} />
+              <Task key={index} task={task} toggleComplete={toggleComplete} deleteTask={deleteTask}/>
             ))}
           </ul>
-          <h4 className="text-center md:text-sm text-blue-400 md:p-3">
-            You have {tasks.length} Tasks
+          <h4 className="text-center md:text-sm text-xs p-2 text-blue-400 md:p-2 tracking-wide font-semibold">
+            {tasks.length ? 
+              `You have ${tasks.length} Tasks` : "Drop your first task 📋"
+            }
           </h4>
         </div>
       </div>
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React from "react";
 
-const Task = ({task}) => {
+const Task = ({ task, toggleComplete, deleteTask }) => {
   return (
-    <li className='border-b-2 border-gray-500 rounded-sm md:p-4'>
-      <div className='flex items-center w-full'>
-        <input type="checkbox" className='md:mr-3' />
-        <p className='w-full'>{task}</p>
-        <button><i className="fa-solid fa-trash-can text-lg text-red-400 cursor-pointer hover:scale-125 transition-all duration-300 ease-in-out"></i></button>
+    <li
+      className={`border-b-2 border-gray-500 rounded-sm md:p-4 p-2 ${
+        task.completed ? "text-gray-500 bg-black/90" : ""
+      }`}
+    >
+      <div className="flex items-center w-full">
+        <input
+          onChange={() => toggleComplete(task)}
+          type="checkbox"
+          className="md:mr-3 mr-2 focus:outline-none focus:ring-0"
+          defaultChecked={task.completed}
+        />
+        <p
+          onClick={() => toggleComplete(task)}
+          className={`w-full text-sm ${task.completed ? "line-through" : ""}`}
+        >
+          {task.text}
+        </p>
+        <button onClick={() => deleteTask(task.id)}>
+          <i className="fa-solid fa-trash-can text-sm md:text-lg text-red-400 cursor-pointer hover:scale-125 transition-all duration-300 ease-in-out"></i>
+        </button>
       </div>
     </li>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
